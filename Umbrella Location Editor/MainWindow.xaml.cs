1f3313d6eec6bf4602e260954a96e69b8f329fac
Elton Silva <eltoncarsilva73@gmail.com>
2020-06-02 22:44:55 -0300

    first commit

--- Umbrella Location Editor/MainWindow.xaml.cs
+++ Umbrella Location Editor/MainWindow.xaml.cs
@@ -23,6 +23,96 @@ namespace Umbrella_Location_Editor
         public MainWindow()
         {
             InitializeComponent();
+            this.Title = "UmbrellaToolKit Editor [0.0.1] Beta";
+            this.Grid = new Grid();
+            this.CreateMenu();
+            //this.CreateTable();
+
+
+            this.Content = this.Grid;
+        }
+
+        public void CreateMenu()
+        {
+            
+            DockPanel DockPanel = new DockPanel();
+            DockPanel.Background = Brushes.White;
+
+            ToolBarTray ToolBarTray = new ToolBarTray();
+            ToolBarTray.Background = Brushes.White;
+
+            ToolBar ToolBar = new ToolBar();
+            ToolBar.Background = Brushes.White;
+
+            Button NewBtn = new Button();
+            NewBtn.Name = "New";
+            NewBtn.Background = Brushes.White;
+            NewBtn.Content = "New";
+            ToolBar.Items.Add(NewBtn);
+
+            Button OpenBtn = new Button();
+            OpenBtn.Name = "Open";
+            OpenBtn.Background = Brushes.White;
+            OpenBtn.Content = "Open";
+            ToolBar.Items.Add(OpenBtn);
+
+            Button SaveBtn = new Button();
+            SaveBtn.Name = "Save";
+            SaveBtn.Background = Brushes.White;
+            SaveBtn.Content = "Save";
+            ToolBar.Items.Add(SaveBtn);
+
+            ToolBarTray.ToolBars.Add(ToolBar);
+            DockPanel.Children.Add(ToolBarTray);
+
+            this.Grid.Children.Add(DockPanel);
+        }
+
+        /// <summary>
+        /// Create a new Table Content
+        /// </summary>
+        Grid Grid;
+        public void CreateTable()
+        {
+            Grid = new Grid();
+
+            int columns = 3;
+            int rows = 3;
+
+            for (int x = 0; x < columns; x++)
+                Grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            for (int y = 0; y < rows; y++)
+            {
+                RowDefinition r = new RowDefinition();
+                r.Height = GridLength.Auto;
+                Grid.RowDefinitions.Add(r);
+            }
+
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    if(x == 0)
+                    {
+                        TextBlock text = new TextBlock();
+                        text.Text = y.ToString();
+                        Grid.SetColumn(text, x);
+                        Grid.SetRow(text, y);
+                        Grid.Children.Add(text);
+                    }
+                    else
+                    {
+                        TextBox tb = new TextBox();
+                        tb.Text = x + " " + y;
+                        Grid.SetColumn(tb, x);
+                        Grid.SetRow(tb, y);
+                        Grid.Children.Add(tb);
+                    }
+                }
+            }
+
+            this.Content = Grid;
         }
     }
 }
